94ee99e7804d7f71ef9f278a7733ee65b83ce3ca
TianTcl <49802993+TianTcl@users.noreply.github.com>
2021-12-12 00:06:40 +0700

    Add workshop/

---
+++ public.cf/workshop/index.php
@@ -0,0 +1,67 @@
+<?php
+    $dirPWroot = str_repeat("../", substr_count($_SERVER['PHP_SELF'], "/")-1);
+	require($dirPWroot."resource/hpe/init_ps.php");
+	$header_title = "Workshop";
+	$header_desc = "การย้อนดูคลิปวีดีโอ";
+?>
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<?php require($dirPWroot."resource/hpe/heading.php"); require($dirPWroot."resource/hpe/init_ss.php"); ?>
+		<style type="text/css">
+			main form input, main form select { background-color: var(--clr-psc-skin-shiny) !important; }
+		</style>
+		<script type="text/javascript">
+			function update() { document.querySelector("form button").disabled = false; }
+			function watch() {
+				go_on(); return false;
+				function go_on() {
+					var data = { remote: true,
+						name: document.querySelector('form [name="name"]').value.trim(),
+						clip: document.querySelector('form [name="clip"]').value.trim()
+					}; if (data.name.length <= 0 || data.name.length > 30) {
+						app.ui.notify(1, [3, "Your nickname is empty or too long"]);
+						$('form [name="name"]').focus(); return false;
+					} if (!/^([ก-๛]{1,30}|[A-Z][a-z]{0,29})$/.test(data.name)) {
+						app.ui.notify(1, [2, "Invalid nickname format"]);
+						$('form [name="name"]').focus(); return false;
+					} if (data.clip.length == 0) {
+						app.ui.notify(1, [3, "No video selected"]);
+						$('form [name="clip"]').focus(); return false;
+					} document.querySelector("form button").disabled = true;
+					$.post("https://inf.bodin.ac.th/e/Pathway-Speech-Contest/resource/php/api", {app: "workshop", cmd: "view", attr: data}, function(res, hsc) {
+						var dat = JSON.parse(res);
+						if (dat.success) $.post("/resource/php/core/override", {app: "workshop", cmd: "view", attr: data}, function(res, hcs) {
+							location = "/recording/"+dat.info;
+						}); else {
+							app.ui.notify(1, dat.reason);
+							document.querySelector("form button").disabled = false;
+						}
+					});
+				}
+			}
+		</script>
+	</head>
+	<body>
+		<?php require($dirPWroot."resource/hpe/header.php"); ?>
+		<main shrink="<?php echo($_COOKIE['sui_open-nt'])??"false"; ?>">
+			<div class="container">
+				<h2>การดูคลิปวีดีโอย้อนหลัง</h2>
+				<form class="form" onChange="update()">
+					<input type="text" name="name" maxlength="30" placeholder="Your nickname">
+					<div class="group">
+						<select name="clip">
+							<option selected value disabled>---Video---</option>
+							<option value="1">1st Workshop (11/12/2021)</option>
+						</select>
+						<button class="blue full-x ripple-click" onClick="return watch()" disabled>Watch</button>
+					</div>
+				</form>
+			</div>
+		</main>
+		<?php require($dirPWroot."resource/hpe/material.php"); ?>
+		<footer>
+			<?php require($dirPWroot."resource/hpe/footer.php"); ?>
+		</footer>
+	</body>
+</html>
---
+++ public.cf/workshop/video_playback.php
@@ -0,0 +1,70 @@
+<?php
+    $dirPWroot = str_repeat("../", substr_count($_SERVER['PHP_SELF'], "/")-1);
+	require($dirPWroot."resource/hpe/init_ps.php");
+	$header_title = "Workshop";
+	$header_desc = "การย้อนดูคลิปวีดีโอ";
+
+	if (!isset($_GET['signature']) || empty(trim($_GET['signature']))|| !isset($_SESSION['event']['workshop-URL'])
+		|| $_SESSION['event']['workshop-URL'] <> trim($_GET['signature'])) header("location /workshop/");
+	$player_secured = false; $home = "workshop/";
+	switch ($_SESSION['event']['workshop-VDO']) {
+		case 1:
+			$player_src = "workshop_2021-12-11"; $player_name = "1st Workshop (11/12/2021)";
+			$player_duration = ""; $player_thumbnail = "https://inf.bodin.ac.th/e/Pathway-Speech-Contest/resource/images/workshop-1.png"; break;
+		default: header("location /e/Pathway-Speech-Contest/workshop/"); break;
+	} $player_src = "https://inf.bodin.ac.th/e/Pathway-Speech-Contest/resource/images/$player_src.mp4";
+?>
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<?php require($dirPWroot."resource/hpe/heading.php"); require($dirPWroot."resource/hpe/init_ss.php"); ?>
+		<style type="text/css">
+			:root {
+				--lt-col: 255, 237, 237;
+				--lt-stroke: 126, 3, 3;
+				--lt-bgcol: 165, 20, 20;
+				--li-dl: 242, 182, 182;
+				--li-dd: 167, 23, 23;
+				--li-dbs: 246, 197, 197;
+				--bar-color: #FF0000;
+				--track-size: 3.45px;
+			}
+			.frame {
+				position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);
+				width: 960px; height: 540px;
+				box-shadow: /*5.75px 5.75px 4.6px 8.05px rgba(0, 0, 0, 0.46)*/0 4px 15px 2px rgba(0,0,0,0.35);
+			}
+			@media only screen and (max-width: 768px) {
+				.frame {
+					transform: translate(-50%, -50%) scale(0.575);
+					width: 640px; height: 360px;
+				}
+			}
+		</style>
+		<script type="text/javascript">
+			// Initialize
+			const record = false;
+			$(document).ready(function() {
+				<?php if(isset($player_secured))echo($player_secured)?"gen_blob();":'is_not_secured();';?>
+				jsinit(where=".player");
+			});
+			function is_not_secured() {
+				document.querySelector(".player div.full.video div.view video").src = "<?php if(isset($player_src))echo $player_src; ?>";
+				$(".player div.full.video div.view div.load-text").text("");
+				$(".player div.full.video div.view div.load-img").hide();
+			}
+		</script>
+	</head>
+	<body>
+		<?php require($dirPWroot."resource/hpe/header.php"); ?>
+		<main shrink="<?php echo($_COOKIE['sui_open-nt'])??"false"; ?>">
+			<div class="frame">
+				<?php include($dirPWroot."resource/php/video_player.php"); ?>
+			</div>
+		</main>
+		<?php require($dirPWroot."resource/hpe/material.php"); ?>
+		<footer>
+			<?php require($dirPWroot."resource/hpe/footer.php"); ?>
+		</footer>
+	</body>
+</html>
